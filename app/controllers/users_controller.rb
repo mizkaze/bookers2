94f8844ee1bca715992d159bfb8a10700559c6f3
mizkaze <watashi03go@gmail.com>
2022-04-07 13:57:16 +0000

    [save]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,11 @@ class UsersController < ApplicationController
   # sign_inしているユーザーのみ入れる
 
   def index
+    # 《投稿フォーム用》
+    @new_book = Book.new
+    @user = current_user
     # 全ユーザーの全投稿を表示
-    @book = Book.find(params[:id])
+    @users = User.all
   end
 
   # 必要なのは特定のユーザー(currentとは限らない)と、その本すべて
@@ -12,10 +15,11 @@ class UsersController < ApplicationController
     # 《投稿フォーム用》
     @new_book = Book.new
     @user = current_user
-    # 既存の本、↑↓同じユーザーとは限らない
+    # ↓URLからユーザーの情報を取得する
     @id_user = User.find(params[:id])
     # ↓一覧表示のためのインスタンス変数(15章)
     @books = @id_user.books
+    # = [変数.モデル名(アソシエーションで関連付られているから使える)]
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,13 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   # “profile_image”という名前で画像を保存できるようになる
 
-  def get_profile_image
-    unless profile_image.attached?
-      file_path = Rails.root.join('/bookers2/app/assets/images/no-image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [100,100]).processed
-  end
+# エラーが出て画像表示できないから、質問できるまで保留
+  # def get_profile_image
+  #   unless profile_image.attached?
+  #     file_path = Rails.root.join('/bookers2/app/assets/images/default-image.jpg')
+  #     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  #   end
+  #   profile_image.variant(resize_to_limit: [width, height]).processed
+  # end
 
 end
