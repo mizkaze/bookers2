9716addfbd0af348f81d96cdddc87ce69b504a51
mizkaze <watashi03go@gmail.com>
2022-04-08 16:54:03 +0000

    [Add]edit,update,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+    # すでに作った(保存されている)本を表示
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
 
