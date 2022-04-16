8fc144770d86fa472bf550f9c131458d13e0f5cd
mizkaze <watashi03go@gmail.com>
2022-04-16 10:04:34 +0000

    [Save]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # バリデーション
-  validates :name, presence: true, uniqueness: true, length: { minimun: 2, maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
   #アソシエーション
